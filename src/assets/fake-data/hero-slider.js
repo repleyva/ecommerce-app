ca8595be21c29d1e06fdc50749b0fb3837383cd6
repleyva <rusvelpassos@gmail.com>
2021-10-25 08:45:42 -0500

    Adicionando assets

---
+++ src/assets/fake-data/hero-slider.js
@@ -0,0 +1,32 @@
+import img1 from "../images/slider/slide_1.png";
+import img2 from "../images/slider/slide_2.png";
+import img3 from "../images/slider/slide_3.png";
+
+const heroSliderData = [
+  {
+    title: "Premium Pima Women's Polo",
+    description:
+      "Referring to class, it is impossible not to mention the pima fabric line. It creates the best quality for any fashion product. The pima yarn is twice as long and thicker than regular cotton yarn thanks to advanced weaving technology. That makes the structure of the polo shirt strong, durable, minimizes ruffles, smooth, colorfast, extremely ensuring the user's health.",
+    img: img1,
+    color: "blue",
+    path: "/catalog/t-shirts-dinosaur-01",
+  },
+  {
+    title: "Fashionable Women's Polo Modal",
+    description:
+      "Modal women's polo shirt uses high-quality environmentally friendly fabric technology to produce exclusive anti-shrink fabric, modal women's polo shirt is a suitable product for those of you who have a dynamic working environment like today.",
+    img: img2,
+    path: "/catalog/t-shirts-dinosaur-02",
+    color: "pink",
+  },
+  {
+    title: "Polo Coolmax Lacoste",
+    description:
+      "Women's polo shirt model is made of coolmax material that feels cool to wear. This coolmax polo shirt design features an ultra-comfortable fit. Creating neatness promises to be a very hot polo shirt in the near future",
+    img: img3,
+    path: "/catalog/t-shirts-dinosaur-03",
+    color: "orange",
+  },
+];
+
+export default heroSliderData;
---
+++ src/assets/fake-data/products.js
@@ -0,0 +1,235 @@
+import product_01_image_01 from "../images/products/product-01 (1).jpg";
+import product_01_image_02 from "../images/products/product-01 (2).jpg";
+
+import product_02_image_01 from "../images/products/product-02 (1).jpg";
+import product_02_image_02 from "../images/products/product-02 (2).jpg";
+
+import product_03_image_01 from "../images/products/product-03 (1).jpg";
+import product_03_image_02 from "../images/products/product-03 (2).jpg";
+
+import product_04_image_01 from "../images/products/product-04 (1).jpg";
+import product_04_image_02 from "../images/products/product-04 (2).jpg";
+
+import product_05_image_01 from "../images/products/product-05 (1).jpg";
+import product_05_image_02 from "../images/products/product-05 (2).jpg";
+
+import product_06_image_01 from "../images/products/product-06 (1).jpg";
+import product_06_image_02 from "../images/products/product-06 (2).jpg";
+
+import product_07_image_01 from "../images/products/product-07 (1).jpg";
+import product_07_image_02 from "../images/products/product-07 (2).jpg";
+
+import product_08_image_01 from "../images/products/product-08 (1).jpg";
+import product_08_image_02 from "../images/products/product-08 (2).jpg";
+
+import product_09_image_01 from "../images/products/product-09 (1).jpg";
+import product_09_image_02 from "../images/products/product-09 (2).jpg";
+
+import product_10_image_01 from "../images/products/product-10 (1).jpg";
+import product_10_image_02 from "../images/products/product-10 (2).jpg";
+
+import product_11_image_01 from "../images/products/product-11 (1).jpg";
+import product_11_image_02 from "../images/products/product-11 (2).jpg";
+
+import product_12_image_01 from "../images/products/product-12 (1).jpg";
+import product_12_image_02 from "../images/products/product-12 (2).jpg";
+
+const products = [
+  {
+    title: "Dinosaur T-shirt 01",
+    price: "189000",
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    categorySlug: "t-shirts",
+    colors: ["white", "red", "orange"],
+    slug: "t-shirts-dinosaur-01",
+    size: ["s", "m", "l", "xl"],
+  },
+  {
+    title: "Dinosaur T-shirt 02",
+    price: "159000",
+    image01: product_02_image_01,
+    image02: product_02_image_02,
+    categorySlug: "t-shirts",
+    colors: ["white", "red", "blue"],
+    slug: "t-shirts-dinosaur-02",
+    size: ["s", "m"],
+  },
+  {
+    title: "Dinosaur T-shirt 03",
+    price: "190000",
+    image01: product_03_image_01,
+    image02: product_03_image_02,
+    categorySlug: "t-shirts",
+    colors: ["white", "red", "orange", "yellow"],
+    slug: "t-shirts-dinosaur-03",
+    size: ["m"],
+  },
+  {
+    title: "Polo T-shirt 04",
+    price: "194000",
+    image01: product_04_image_01,
+    image02: product_04_image_02,
+    categorySlug: "t-shirts",
+    colors: ["white", "orange", "blue"],
+    slug: "t-shirts-polo-04",
+    size: ["xl"],
+  },
+  {
+    title: "Polo t-shirt 05",
+    price: "194000",
+    image01: product_05_image_01,
+    image02: product_05_image_02,
+    categorySlug: "t-shirts",
+    colors: ["white", "pink"],
+    slug: "t-shirts-polo-05",
+    size: ["xxl"],
+  },
+  {
+    title: "Polo t-shirt 06",
+    price: "194000",
+    image01: product_06_image_01,
+    image02: product_06_image_02,
+    categorySlug: "t-shirts",
+    colors: ["black"],
+    slug: "t-shirts-polo-06",
+    size: ["s", "m", "xl"],
+  },
+  {
+    title: "Checkered shirt 07",
+    price: "194000",
+    image01: product_07_image_01,
+    image02: product_07_image_02,
+    categorySlug: "shirt",
+    colors: ["white", "red", "orange", "blue"],
+    slug: "shirt-checkered-07",
+    size: ["l", "xl"],
+  },
+  {
+    title: "Long sleeve shirt 08",
+    price: "194000",
+    image01: product_08_image_01,
+    image02: product_08_image_02,
+    categorySlug: "shirt",
+    colors: ["white", "red", "black"],
+    slug: "shirt-long-sleeve-08",
+    size: ["s", "m", "xl"],
+  },
+  {
+    title: "Long sleeve shirt 09",
+    price: "194000",
+    image01: product_09_image_01,
+    image02: product_09_image_02,
+    categorySlug: "shirt",
+    colors: ["white", "blue"],
+    slug: "shirt-long-sleeve-09",
+    size: ["m"],
+  },
+  {
+    title: "Stylish jeans 10",
+    price: "194000",
+    image01: product_10_image_01,
+    image02: product_10_image_02,
+    categorySlug: "jeans",
+    colors: ["blue", "black"],
+    slug: "stylish-jeans-10",
+    size: ["l"],
+  },
+  {
+    title: "Jeans 11",
+    price: "194000",
+    image01: product_11_image_01,
+    image02: product_11_image_02,
+    categorySlug: "jeans",
+    colors: ["blue", "black"],
+    slug: "jeans-11",
+    size: ["s", "m", "xl"],
+  },
+  {
+    title: "Jeans 12",
+    price: "194000",
+    image01: product_12_image_01,
+    image02: product_12_image_02,
+    categorySlug: "jeans",
+    colors: ["blue"],
+    slug: "jeans-12",
+    size: ["s", "m", "xl"],
+  },
+  {
+    title: "Dinosaur T-shirt 13",
+    price: "189000",
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    categorySlug: "t-shirts",
+    colors: ["white", "red"],
+    slug: "t-shirts-dinosaur-13",
+    size: ["s", "m", "xl"],
+  },
+  {
+    title: "Dinosaur T-shirt 14",
+    price: "159000",
+    image01: product_02_image_01,
+    image02: product_02_image_02,
+    categorySlug: "t-shirts",
+    colors: ["white", "blue"],
+    slug: "t-shirts-dinosaur-14",
+    size: ["s", "m"],
+  },
+  {
+    title: "Dinosaur T-shirt 15",
+    price: "190000",
+    image01: product_03_image_01,
+    image02: product_03_image_02,
+    categorySlug: "t-shirts",
+    colors: ["red", "blue"],
+    slug: "t-shirts-dinosaur-15",
+    size: ["xl"],
+  },
+  {
+    title: "Long sleeve shirts 16",
+    price: "194000",
+    image01: product_08_image_01,
+    image02: product_08_image_02,
+    categorySlug: "shirt",
+    colors: ["blue", "black"],
+    slug: "shirt-long-sleeve-16",
+    size: ["m", "xl"],
+  },
+  {
+    title: "Long sleeve shirts 17",
+    price: "194000",
+    image01: product_09_image_01,
+    image02: product_09_image_02,
+    categorySlug: "shirt",
+    colors: ["white", "blue"],
+    slug: "shirt-long-sleeve-17",
+    size: ["s", "l", "xl"],
+  },
+  {
+    title: "Stylish jeans 18",
+    price: "194000",
+    image01: product_10_image_01,
+    image02: product_10_image_02,
+    categorySlug: "jeans",
+    colors: ["blue", "black"],
+    slug: "stylish-jeans-18",
+    size: ["s", "m", "l", "xl"],
+  },
+  // 18 products
+];
+
+const getAllProducts = () => products;
+
+const getProducts = (count) => {
+  const max = products.length - count;
+  const min = 0;
+  const start = Math.floor(Math.random() * (max - min) + min);
+  return products.slice(start, start + count);
+};
+
+const productData = {
+  getAllProducts,
+  getProducts,
+};
+
+export default productData;
