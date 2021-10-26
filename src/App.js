30bb3c0786601e792c4a2c26a4b44bf1afb7fc22
repleyva <rusvelpassos@gmail.com>
2021-10-26 11:02:17 -0500

    Terminando las configuraciones iniciales

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Routes from "./routes/Routes";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Route
+        render={(props) => (
+          <div>
+            <div className="container">
+              <Header {...props} />
+              <div className="main">
+                <Routes />
+              </div>
+            </div>
+            <Footer />
+          </div>
+        )}
+      ></Route>
+    </Router>
+  );
 };
 
 export default App;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,18 @@
-const Routes = () => {
-	return <div>Routes</div>;
-}
+import { Route, Switch } from "react-router";
+import Cart from "../pages/Cart";
+import Catalog from "../pages/Catalog";
+import Home from "../pages/Home";
+import Product from "../pages/Product";
 
-export default Routes
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/catalog/:slug" component={Product} />
+      <Route exact path="/catalog" component={Catalog} />
+      <Route exact path="/cart" component={Cart} />
+    </Switch>
+  );
+};
+
+export default Routes;
