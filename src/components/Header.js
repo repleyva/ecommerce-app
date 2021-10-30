39ff62bdc2cea48ecf0824d268ea2eb7c405b972
repleyva <rusvelpassos@gmail.com>
2021-10-30 12:31:52 -0500

    header laptop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,92 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
+import logo from "../assets/images/Logo-2.png";
+
+const mainNav = [
+  {
+    display: "Home",
+    path: "/",
+  },
+  {
+    display: "Catalog",
+    path: "/catalog",
+  },
+  {
+    display: "Accessories",
+    path: "/accessories",
+  },
+  {
+    display: "Contact",
+    path: "/contact",
+  },
+];
 
 const Header = () => {
-	return (
-		<div>
-			header
-		</div>
-	)
-}
+  const { pathname } = useLocation();
+  const activeNav = mainNav.findIndex((e) => e.path === pathname);
+  const headerRef = useRef(null);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("shrink");
+      } else {
+        headerRef.current.classList.remove("shrink");
+			}
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <header className="header" ref={headerRef}>
+      <div className="container">
+        <div className="header__logo">
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+        </div>
+        <div className="header__menu">
+          <div className="header__menu__mobile-toggle">
+            <i className="bx bx-menu-alt-left"></i>
+          </div>
+          <div className="header__menu__left">
+            <div className="header__menu__left__close">
+              <i className="bx bx-chevron-left"></i>
+            </div>
+            {mainNav.map((item, index) => (
+              <div
+                key={index}
+                className={`header__menu__item header__menu__left__item ${
+                  index === activeNav && "active"
+                }`}
+              >
+                <Link to={item.path}>
+                  <span>{item.display}</span>
+                </Link>
+              </div>
+            ))}
+          </div>
+          <div className="header__menu__right">
+            <div className="header__menu__item header__menu__right__item">
+              <i className="bx bx-search"></i>
+            </div>
+            <div className="header__menu__item header__menu__right__item">
+              <Link to="/cart">
+                <i className="bx bx-shopping-bag"></i>
+              </Link>
+            </div>
+            <div className="header__menu__item header__menu__right__item">
+              <i className="bx bx-user"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
