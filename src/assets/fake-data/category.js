299a387a7f2c7962f2f97725627bf9c71743d8ef
repleyva <rusvelpassos@gmail.com>
2021-11-03 08:25:33 -0500

    agregando catalogo

---
+++ src/assets/fake-data/category.js
@@ -0,0 +1,16 @@
+const category = [
+  {
+    display: "T-shirts",
+    categorySlug: "t-shirts",
+  },
+  {
+    display: "Shirt",
+    categorySlug: "shirt",
+  },
+  {
+    display: "Jeans",
+    categorySlug: "jeans",
+  },
+];
+
+export default category
--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,7 +1,44 @@
+import category from "../assets/fake-data/category";
+import productData from "../assets/fake-data/products";
+import Grid from "../components/Grid";
 import Helmet from "../components/Helmet";
+import ProductCard from "../components/ProductCard";
 
 const Catalog = () => {
-	return <Helmet title="Catalog">Catalog</Helmet>;
-}
+  return (
+    <Helmet title="Catalog">
+      <div className="catalog">
+        <div className="catalog__filter">
+          <div className="catalog__filter__widget">
+            <div className="catalog__filter__widget__title">
+              product portfolio
+            </div>
+            <div className="catalog__filter__widget__content">
+              {category.map((item, index) => (
+                <p className="catalog__filter__widget__content__item">
+                  {item.display}
+                </p>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="catalog__content">
+          <Grid col={3} mdCol={2} smCol={1} gap={20}>
+            {productData.getAllProducts().map((item, index) => (
+              <ProductCard
+                key={index}
+                img01={item.image01}
+                img02={item.image02}
+                name={item.title}
+                price={Number(item.price)}
+                slug={item.slug}
+              />
+            ))}
+          </Grid>
+        </div>
+      </div>
+    </Helmet>
+  );
+};
 
-export default Catalog
+export default Catalog;
