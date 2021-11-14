28734be77a290e1456ccf7d978c5fe5cbf82c116
repleyva <rusvelpassos@gmail.com>
2021-11-14 13:19:10 -0500

    store con producto

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productModalReducer from "./product-modal/productModalSlice";
+
+export const store = configureStore({
+  reducer: {
+    productModal: productModalReducer,
+  },
+});
