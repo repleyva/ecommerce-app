f96d81e0d5a4c9a9b192af1f40c45cadd44b2837
repleyva <rusvelpassos@gmail.com>
2021-11-14 13:20:03 -0500

    leyendo el producto en redux con use Selector

--- src/components/ProductViewModal.js
+++ src/components/ProductViewModal.js
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import ProductView from "./ProductView";
 import Button from "./Button";
+import { remove } from "../redux/product-modal/productModalSlice";
 import productData from "../assets/fake-data/products";
 
 const ProductViewModal = () => {
 	// recuperamos el valor del slug mandado desde el productCard
+  const productSlug = useSelector((state) => state.productModal.value);
+	// usaremos dispatch para elmiminar el slug
+  const dispatch = useDispatch();
+
+  const [product, setProduct] = useState(undefined);
+
+  useEffect(() => {
+		// llenamos el producto buscando en producData el que coincida con el slug
+    setProduct(productData.getProductBySlug(productSlug));
+  }, [productSlug]);
 
   return (
     <div
@@ -13,6 +25,7 @@ const ProductViewModal = () => {
       <div className="product-view__modal__content">
         <ProductView product={product} />
         <div className="product-view__modal__content__close">
+          <Button size="sm" onClick={() => dispatch(remove())}>
             close
           </Button>
         </div>
