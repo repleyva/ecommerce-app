8d3b450bda17489d54da309f64b4e378644aacc4
repleyva <rusvelpassos@gmail.com>
2021-11-14 13:18:01 -0500

    Agregando view modal a la app, ocultandolo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import ProductViewModal from "./components/ProductViewModal";
 import Routes from "./routes/Routes";
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
               </div>
             </div>
             <Footer />
+						<ProductViewModal/>
           </div>
         )}
       ></Route>
